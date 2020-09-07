e394b44ffd5cfceba2cecdeeb0a7d6150a25eaf2
shanesully <shanesully01@gmail.com>
2020-09-07 21:02:43 +0100

    Initial commit - some h2 test code

---
+++ src/main/java/com/shanedev/h2testing/persistence/entities/Student.java
@@ -0,0 +1,14 @@
+package com.shanedev.h2testing.persistence.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String passportNumber;
+}
---
+++ src/test/java/com/shanedev/h2testing/H2TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanedev.h2testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
